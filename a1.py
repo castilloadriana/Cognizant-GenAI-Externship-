09160054d10c293b5f5574b047deea8b036abf33
adriana <castilloadriana412@gmail.com>
2025-05-24 16:22:12 -0400

    assignment 1

---
+++ a1.py
@@ -0,0 +1,39 @@
+name = "Adriana"
+age= 21
+height= 5.4
+
+print("Task 1")
+print ("Hey there, my name is ", name , "! I’m ", age , " years old and ", height, " feet tall.")
+
+
+
+
+
+
+print("Task 2")
+num1= 8
+num2= 5
+
+#the two variables are added 
+print("The sum of 8 and 5 is", num1 + num2)
+
+#the two variables are substracted 
+print("The substraction of 8 and 5 is", num1 - num2)
+
+#the two variables are multiplied 
+print("The multiplication of 8 and 5 is", num1 * num2)
+
+#the two variables are divided
+print("The division of 8 and 5 is", num1 / num2)
+
+
+
+print("Task 3")
+num = int(input("Please enter a number: "))
+
+if num > 0: 
+    print("The number is positive. Awesome!")
+elif num < 0 :
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
